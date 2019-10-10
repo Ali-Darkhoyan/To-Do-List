61f8a54201a9b26d7b3cfc19d13616da44231149
Ali-Darkhoyan <54389593+Ali-Darkhoyan@users.noreply.github.com>
2019-10-09 20:42:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let button = document.getElementById("enter");
+let input = document.getElementById("userinput");
+let input1 = document.getElementById("userinput1");
+let input2 = document.getElementById("userinput2");
+let ul = document.querySelector("ul");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
